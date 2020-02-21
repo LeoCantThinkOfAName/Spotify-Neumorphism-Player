c882cf81c6fef91da6cb31aaddb33ba6c3d1c8be
LeoCantThinkofAName <i7851@hotmail.com>
2020-02-21 17:40:00 +0800

    use my playlist :D

--- src/utils/playMusic.ts
+++ src/utils/playMusic.ts
@@ -2,14 +2,13 @@ const playMusic = (device_id: string, token: string) => {
   fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
     method: "PUT",
     // @ts-ignore
-    body:
-      '{"context_uri":"spotify:album:1qxLJGTmKJZVXzqKwLdzxD?si=LnYriphxQCidZMFp5WUuQw"}',
+    body: '{"context_uri":"spotify:playlist:4GMQCtlmLaR8Any2pmMPPw"}',
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
       Accept: "application/json"
     }
-  });
+  }).then((data: any) => console.log(data));
 };
 
 export default playMusic;
