82e7bd32b8196a3e0865c8bbc5422d783a3953b7
leocantthinkofanamae <i7851@hotmail.com>
2020-02-23 19:13:26 +0800

    move init function out of App.tsx

---
+++ src/utils/initPlayer.ts
@@ -0,0 +1,39 @@
+import playMusic from "../utils/playMusic";
+
+type Func = ({
+  token,
+  playlistId,
+}: {
+  token: string | null;
+  playlistId: string;
+}) => void;
+
+const initPlayer: Func = ({ token, playlistId }) => {
+  if (!token) return;
+
+  const script = document.createElement("script");
+  script.src = "https://sdk.scdn.co/spotify-player.js";
+  script.charset = "utf-8";
+  script.async = true;
+  script.onload = () => {
+    window.onSpotifyPlayerAPIReady = () => {
+      const player = new window.Spotify.Player({
+        name: "web-player",
+        getOAuthToken: (callback: any) => callback(token),
+      });
+
+      player.on("ready", ({ device_id }: { device_id: string }) => {
+        playMusic({
+          deviceId: device_id,
+          token,
+          playlistId,
+        });
+      });
+
+      player.connect();
+    };
+  };
+  document.body.append(script);
+};
+
+export default initPlayer;
