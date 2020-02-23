ccf77d6f16603e30884e38098b6eddabae246407
leocantthinkofanamae <i7851@hotmail.com>
2020-02-23 19:12:27 +0800

    add usePlaylist custom hooks

---
+++ src/hooks/usePlaylist.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+// types
+import { IPlaylist } from "../types/spotify";
+import { MyPlaylist } from "../types/playlist";
+
+// utilities
+import transformPlaylist from "../utils/transformPlaylist";
+
+type Hook = ({
+  token,
+  playlistId,
+}: {
+  token: string | null;
+  playlistId: string;
+}) => MyPlaylist[] | null;
+
+// playlistId = 4GMQCtlmLaR8Any2pmMPPw;
+
+const usePlaylist: Hook = ({ token, playlistId }) => {
+  const [playlist, setPlaylist] = useState<MyPlaylist[] | null>(null);
+
+  useEffect(() => {
+    if (token) {
+      fetch(
+        `https://api.spotify.com/v1/playlists/${playlistId}/tracks?fields=items(track(id%2Cname%2Cduration_ms%2Calbum(artists%2Chref%2Cimages%2Cname)))`,
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((data: any) => data.json())
+        .then((res: IPlaylist) => setPlaylist(transformPlaylist(res)));
+    }
+  }, [token, playlistId]);
+
+  if (playlist) {
+    return playlist;
+  }
+
+  return null;
+};
+
+export default usePlaylist;
