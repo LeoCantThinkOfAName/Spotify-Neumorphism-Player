a0eadc20c5376d0d9da82c4f613072ed0f525683
LeoCantThinkofAName <i7851@hotmail.com>
2020-02-24 11:51:28 +0800

    adopt new contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useReducer } from "react";
 
 // context
-import { GlobalContext } from "./contexts/GlobalContext";
+import { ThemeContext, SpotifyContext } from "./contexts/GlobalContext";
 
 // components
 import Screen from "./components/Screen/index";
@@ -18,7 +18,7 @@ import spotifyReducer from "./reducers/SpotifyReducer";
 // initial context
 import {
   initialTheme,
-  initialSpotifyContext,
+  initialSpotifyContext
 } from "./contexts/initialContexts";
 
 // utilities
@@ -26,43 +26,45 @@ import initPlayer from "./utils/initPlayer";
 
 const App = () => {
   const [{ theme }, dispatchTheme] = useReducer(themeReducer, initialTheme);
-  const [{ token }, dispatchSpotify] = useReducer(
+  const [{ token, playlistId }, dispatchSpotify] = useReducer(
     spotifyReducer,
     initialSpotifyContext
   );
 
   useEffect(() => {
     if (!token) return;
-    initPlayer({ token, playlistId: "4GMQCtlmLaR8Any2pmMPPw" });
+    initPlayer({ token, playlistId });
   }, [token]);
 
   return (
-    <GlobalContext.Provider
-      value={{ token, theme, dispatchTheme, dispatchSpotify }}
-    >
-      <div className="App">
-        <Palette />
-        <Body bg={theme}>
-          <Cord color={theme} />
-          <Screen
-            height={260}
-            width={240}
-            powerOn={token ? true : false}
-          ></Screen>
-          <Container direction="row">
-            <Button size={50} bg={theme} title="Previous track">
-              Prev
-            </Button>
-            <Button size={70} bg={theme} title="Play/Pause">
-              Play
-            </Button>
-            <Button size={50} bg={theme} title="Next track">
-              Next
-            </Button>
-          </Container>
-        </Body>
-      </div>
-    </GlobalContext.Provider>
+    <ThemeContext.Provider value={{ theme, dispatchTheme }}>
+      <SpotifyContext.Provider value={{ token, playlistId, dispatchSpotify }}>
+        <div className="App">
+          <Palette />
+          <Body bg={theme}>
+            <Cord color={theme} />
+            <Screen
+              height={260}
+              width={240}
+              powerOn={token ? true : false}
+              token={token}
+              playlistId={playlistId}
+            ></Screen>
+            <Container direction="row">
+              <Button size={50} bg={theme} title="Previous track">
+                Prev
+              </Button>
+              <Button size={70} bg={theme} title="Play/Pause">
+                Play
+              </Button>
+              <Button size={50} bg={theme} title="Next track">
+                Next
+              </Button>
+            </Container>
+          </Body>
+        </div>
+      </SpotifyContext.Provider>
+    </ThemeContext.Provider>
   );
 };
 
